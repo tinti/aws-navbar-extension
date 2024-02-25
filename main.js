fd0848a18033e096eb9425ba4d7737c581f56719
Vinicius Tinti <viniciustinti@gmail.com>
2024-02-25 16:54:25 -0300

    feat: update only the end of the nav bar

--- main.js
+++ main.js
@@ -208,7 +208,7 @@ chrome.storage.local.get("config", (c) => {
       // region header background
       if (config["background"] !== "disabled") {
         document.querySelector(
-          "#awsc-navigation-container>div>header>nav"
+          "#awsc-navigation__more-menu--list"
         ).style.background = colors[region]["background"];
       }
 
